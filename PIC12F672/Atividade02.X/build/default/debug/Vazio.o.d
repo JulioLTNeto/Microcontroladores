d9a7816715b4d61982501e03c03da037dee126aa
JulioLTNeto <julioleite.t.n@gmail.com>
2022-09-14 13:30:05 -0300

    Atividades em assembly do pic 12F672

---
+++ PIC12F672/Atividade02.X/build/default/debug/Vazio.o.d
@@ -0,0 +1,3 @@
+build/default/debug/Vazio.o:  \
+	C:/Users/julio/MPLABXProjects/Atividade02.X/Vazio.ASM  \
+
---
+++ PIC12F672/Atividade3.X/build/default/debug/Atividade03.o.d
@@ -0,0 +1,3 @@
+build/default/debug/Atividade03.o:  \
+	C:/Users/julio/MPLABXProjects/Atividade3.X/Atividade03.ASM  \
+
