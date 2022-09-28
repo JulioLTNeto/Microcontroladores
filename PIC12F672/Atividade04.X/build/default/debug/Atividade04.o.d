1fac5c5d3e2f70df38a8a747ccdd7ba901e9fa95
JulioLTNeto <julioleite.t.n@gmail.com>
2022-09-28 18:00:45 -0300

    Adiçao da atividade04

---
+++ PIC12F672/Atividade04.X/build/default/debug/Atividade04.o.d
@@ -0,0 +1,3 @@
+build/default/debug/Atividade04.o:  \
+	C:/Users/julio/MPLABXProjects/Atividade04.X/Atividade04.ASM  \
+
---
+++ PIC12F672/Atividade04.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Atividade04.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Atividade04.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=atividade04.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/atividade04.x.tar
